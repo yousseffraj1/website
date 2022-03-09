9661e3b7bc217cc099fc1d86bdd7232ae31620f8
yousseffraj1 <benfraj.youssef@esprit.tn>
2022-03-09 00:35:25 -0800

    map

---
+++ function.h
@@ -0,0 +1,22 @@
+#ifndef Text_H
+#define Text_H
+#include <stdio.h>
+
+#include <SDL/SDL.h> 
+#include <SDL/SDL_image.h> 
+
+
+typedef struct 
+{
+   SDL_Surface * miniture;
+   SDL_Rect posminimap;
+   SDL_Surface * bonhomme;
+   SDL_Rect posbonhomme; 
+}minimap;
+
+
+void afficherminimap (minimap m, SDL_Surface * screen);
+void MAJMinimap(SDL_Rect posJoueur , minimap * m, SDL_Rect camera, int redimensionnement);
+void Liberer (minimap * m);
+#endif
+
